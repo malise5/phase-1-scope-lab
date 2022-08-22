9a9081f31d5bcac37313b07f3bc84004f25e489c
Halkano <kudezx@gmail.com>
2022-08-22 18:47:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+//console.log(customerName);
+const leastFavoriteCustomer = 'halkano';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+ // console.log(upperCaseCustomerName());
+
+ function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+// console.log(setBestCustomer());
+
+ function overwriteBestCustomer (newFavorite){
+    bestCustomer = newFavorite;
+}
+
+
+function changeLeastFavoriteCustomer(newleastFavoriteCustomer){
+    return leastFavoriteCustomer = newleastFavoriteCustomer;
+ 
+}
